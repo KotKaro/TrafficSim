e1819d5065eddf671834c69158e128b3f2a12f30
Karol Kot <karol.kot@aerdata.com>
2023-11-29 21:43:43 +0100

    Router finished

--- src/vehicle/router.py
+++ src/vehicle/router.py
@@ -1,13 +1,15 @@
+import heapq
 import sys
 from collections import deque
 from random import Random
-from typing import List, Deque
+from typing import List, Deque, Dict, Set, Tuple
 
 from src.flow.route import Route
 from src.roadnet.drivable import Drivable
 from src.roadnet.lane import Lane
 from src.roadnet.lane_link import LaneLink
 from src.roadnet.road import Road
+from src.vehicle.router_type import RouterType
 from src.vehicle.vehicle import Vehicle
 
 
@@ -27,6 +29,10 @@ class Router:
 
         self.i_cur_road: Road = self.route[0]
         self.planned: Deque[Drivable] = deque()
+        self.type = RouterType.LENGTH
+
+    def get_first_road(self) -> Road:
+        return self.anchor_points[0]
 
     def get_first_drivable(self) -> Drivable:
         lanes: List[Lane] = self.route[0].get_lane_pointers()
@@ -77,15 +83,6 @@ class Router:
                         self.route[self.route.index(tmpCurRoad) + 2])) > 0]
                     return self.select_lane_link(cur_lane, candidateLaneLinks)
 
-    def select_lane_link(self, cur_lane: Lane, lane_links: List[LaneLink]) -> LaneLink | None:
-        if len(lane_links) == 0:
-            return None
-
-        return lane_links[self.select_lane_index(cur_lane, [x.get_end_lane() for x in lane_links])]
-
-    def set_vehicle(self, vehicle: Vehicle):
-        self.vehicle = vehicle
-
     def update(self) -> None:
         cur_drivable = self.vehicle.get_cur_drivable()
         if cur_drivable.is_lane():
@@ -104,6 +101,127 @@ class Router:
 
     def on_last_road(self) -> bool:
         return self.is_last_road(self.vehicle.get_cur_drivable())
+
+    def on_valid_lane(self) -> bool:
+        return (self.get_next_drivable() is None and self.on_last_road() is False) is False
+
+    def get_valid_lane(self, cur_lane: Lane) -> Lane | None:
+        if self.is_last_road(cur_lane):
+            return None
+
+        next_road: Road = self.i_cur_road
+        self.i_cur_road = self.route[self.route.index(self.i_cur_road) + 1]
+        min_diff: int = len(cur_lane.belong_road.get_lanes())
+
+        chosen: Lane
+        for lane in cur_lane.belong_road.get_lane_pointers():
+            cur_lane_diff: int = lane.lane_index - cur_lane.lane_index
+            if len(lane.get_lane_links_to_road(next_road)) > 0 and abs(cur_lane_diff) < min_diff:
+                min_diff = abs(cur_lane_diff)
+                chosen = lane
+
+        assert chosen.belong_road == cur_lane.belong_road
+        return chosen
+
+    def set_vehicle(self, vehicle: Vehicle):
+        self.vehicle = vehicle
+
+    def dijkstra(self, start: Road, end: Road, buffer: List[Road]) -> bool:
+        dis = {start: 0}
+        from_road = {}
+        visited = set()
+        success = False
+
+        queue = []
+        heapq.heappush(queue, (0, start))  # Priority queue with (distance, road)
+
+        while queue:
+            cur_dis, cur_road = heapq.heappop(queue)
+            if cur_road == end:
+                success = True
+                break
+
+            if cur_road in visited:
+                continue
+            visited.add(cur_road)
+
+            for adj_road in cur_road.get_end_intersection().get_roads():
+                if not cur_road.connected_to_road(adj_road):
+                    continue
+
+                new_dis = cur_dis
+                if self.type == RouterType.LENGTH:
+                    new_dis += adj_road.average_length()
+                elif self.type == RouterType.DURATION:
+                    avg_dur = adj_road.get_average_duration()
+                    if avg_dur < 0:
+                        avg_dur = adj_road.get_length() / self.vehicle.get_max_speed()
+                    new_dis += avg_dur
+
+                if adj_road not in dis or new_dis < dis[adj_road]:
+                    from_road[adj_road] = cur_road
+                    dis[adj_road] = new_dis
+                    heapq.heappush(queue, (new_dis, adj_road))
+
+        path = []
+        iter_road = end
+        while iter_road != start and iter_road in from_road:
+            path.append(iter_road)
+            iter_road = from_road[iter_road]
+        path.append(start)
+
+        buffer.extend(list(reversed(path)))
+
+        return success
+
+    def update_shortest_path(self) -> bool:
+        self.planned.clear()
+        self.route.clear()
+        self.route.append(self.anchor_points[0])
+
+        for i in range(1, len(self.anchor_points)):
+            if self.anchor_points[i - 1] == self.anchor_points[i]:
+                continue
+
+            if self.dijkstra(self.anchor_points[i - 1], self.anchor_points[i], self.route) is False:
+                return False
+
+        if len(self.route) <= 1:
+            return False
+
+        self.i_cur_road = self.route[0]
+        return True
+
+    def set_route(self, anchor: List[Road]) -> bool:
+        if self.vehicle.get_cur_drivable().is_lane_link():
+            return False
+
+        cur_road = self.i_cur_road
+        backup_anchor_points = self.anchor_points.copy()
+        backup_route = self.route.copy()
+
+        self.anchor_points.clear()
+        self.anchor_points = [cur_road] + anchor
+
+        result: bool = self.update_shortest_path()
+        if result and self.on_valid_lane():
+            return True
+
+        self.anchor_points = backup_anchor_points
+        self.route = backup_route
+
+        self.planned.clear()
+        self.i_cur_road = next((i for i, road in enumerate(self.route) if road == cur_road), len(self.route))
+        return False
+
+    def select_lane_link(self, cur_lane: Lane, lane_links: List[LaneLink]) -> LaneLink | None:
+        if len(lane_links) == 0:
+            return None
+
+        return lane_links[self.select_lane_index(cur_lane, [x.get_end_lane() for x in lane_links])]
+
+    def get_following_roads(self) -> List[Road]:
+        return self.route[self.i_cur_road:]
 
     def select_lane_index(self, cur_lane: Lane, lanes: List[Lane]) -> int:
         assert len(lanes) > 0
---
+++ src/vehicle/router_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class RouterType(Enum):
+    LENGTH = 1
+    DURATION = 2
+    DYNAMIC = 3
